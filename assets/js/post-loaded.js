18ddc1bcf8adfaa67b609a2cf9cb5ab4b97d31c5
Jorge Benito Ugás <147073922+DoctorBIOS1990@users.noreply.github.com>
2026-03-06 15:36:08 -0600

    Add JS, CSS

---
+++ assets/js/post-loaded.js
@@ -0,0 +1,13 @@
+// Before loaded page
+document.addEventListener("DOMContentLoaded", function () {
+    let resources = `
+    <link rel="stylesheet" href="assets/css/fonts.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    `;
+    document.head.insertAdjacentHTML('beforeend', resources);
+});
+
+// Languages
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'es'}, 'google_translate_element');
+}

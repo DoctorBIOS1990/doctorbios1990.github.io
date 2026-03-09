212dfa7a1912c816b0b61b6f4a208f78e4ba3d56
Jorge Benito Ugás <147073922+DoctorBIOS1990@users.noreply.github.com>
2026-03-08 21:39:47 -0600

    Add more SVGs

--- assets/js/post-loaded.js
+++ assets/js/post-loaded.js
@@ -1,11 +1,12 @@
 // Before loaded page
+/*
 document.addEventListener("DOMContentLoaded", function () {
     let resources = `
     <link rel="stylesheet" href="assets/css/fontawesome.css">
     `;
     document.head.insertAdjacentHTML('beforeend', resources);
 });
-
+*/
 // Languages
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'es'}, 'google_translate_element');
